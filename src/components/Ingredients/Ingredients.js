6a1fc84e3282185ba76aae9de05517235b09a259
ryujignh <ryujignh@gmail.com>
2020-05-05 12:12:44 +0900

    Fetch ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
@@ -6,6 +6,24 @@ import Search from './Search';
 
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
+
+    useEffect(() => {
+        fetch('https://react-hooks-update-9d1bf.firebaseio.com/ingredients.json')
+            .then(response => response.json())
+            .then(responseData => {
+                const loadedIngredients = [];
+                for (const key in responseData) {
+                    loadedIngredients.push({
+                        id: key,
+                        title: responseData[key].title,
+                        amount: responseData[key].amount,
+                    });
+                }
+                setUserIngredients(loadedIngredients);
+            });
+    //    いつ呼ばれるか決めないとrenderの度に呼ばれてinifinite loopになるので[]を渡してあげる
+    }, []);
+
 
     const addIngredientHandler = ingredient => {
         fetch('https://react-hooks-update-9d1bf.firebaseio.com/ingredients.json', {
