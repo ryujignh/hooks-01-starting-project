9d2a4f712180f9a94cb5f95fec4d3c0fe8398ce1
ryujignh <ryujignh@gmail.com>
2020-05-06 15:08:44 +0900

    Remove fetch ingredients as it's called from search

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,23 +7,6 @@ import Search from './Search';
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
 
-    useEffect(() => {
-        fetch('https://react-hooks-update-9d1bf.firebaseio.com/ingredients.json')
-            .then(response => response.json())
-            .then(responseData => {
-                const loadedIngredients = [];
-                for (const key in responseData) {
-                    loadedIngredients.push({
-                        id: key,
-                        title: responseData[key].title,
-                        amount: responseData[key].amount,
-                    });
-                }
-                setUserIngredients(loadedIngredients);
-            });
-        //    いつ呼ばれるか決めないとrenderの度に呼ばれてinifinite loopになるので[]を渡してあげる
-    }, []);
-
     const filteredIngredientsHandler = useCallback(filteredIngredients => {
         setUserIngredients(filteredIngredients);
     }, []);
